55cce9bb238913f4a726adbadac68a86363e8f30
vuphuong1794 <phuongvu1794@gmail.com>
2025-05-14 18:01:27 +0700

    before pull master

--- app/src/main/java/com/hellodoc/healthcaresystem/retrofit/RetrofitInstance.kt
+++ app/src/main/java/com/hellodoc/healthcaresystem/retrofit/RetrofitInstance.kt
@@ -21,8 +21,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
-//    private const val BASE_URL = "http://192.168.222.22:4000"
-    private const val BASE_URL = "http://192.168.100.5:3000"
+    private const val BASE_URL = "http://172.20.10.3:3000"
+    //private const val BASE_URL = "http://192.168.100.5:3000"
     //private const val BASE_URL = "https://healthcare-backend-yc39.onrender.com"
 
     private val client = OkHttpClient.Builder()
