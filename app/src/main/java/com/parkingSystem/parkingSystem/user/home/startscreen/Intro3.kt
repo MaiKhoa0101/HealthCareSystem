df44c9772e88a261668114d35959d5e921599264
PolyEtilen1909 <nguyenhongtonpy2004@gmail.com>
2025-10-20 20:30:20 +0700

    done ui start screen and signin + signup

--- app/src/main/java/com/parkingSystem/parkingSystem/user/home/startscreen/Intro3.kt
+++ app/src/main/java/com/parkingSystem/parkingSystem/user/home/startscreen/Intro3.kt
@@ -2,18 +2,42 @@ package com.parkingSystem.parkingSystem.user.home.startscreen
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.parkingSystem.core.common.activity.BaseActivity
 import com.parkingSystem.parkingSystem.R
+import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.request.ImageRequest
 
 class Intro3 : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_intro3)
+
+        val gifView = findViewById<ImageView>(R.id.gifImageView)
+
+        val imageLoader = ImageLoader.Builder(this)
+            .components {
+                if (android.os.Build.VERSION.SDK_INT >= 28) {
+                    add(ImageDecoderDecoder.Factory())
+                } else {
+                    add(GifDecoder.Factory())
+                }
+            }
+            .build()
+
+        val request = ImageRequest.Builder(this)
+            .data(R.drawable.gifforcar)
+            .target(gifView)
+            .build()
+
+        imageLoader.enqueue(request)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.intro3)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
