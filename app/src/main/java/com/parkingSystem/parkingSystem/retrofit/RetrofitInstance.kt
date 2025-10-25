35077d2b09e4bc502e098a4696a5948b2e8b8f7d
MaiKhoa0101 <maikhoa2015@gmail.com>
2025-10-26 01:26:28 +0700

    ok

--- app/src/main/java/com/parkingSystem/parkingSystem/retrofit/RetrofitInstance.kt
+++ app/src/main/java/com/parkingSystem/parkingSystem/retrofit/RetrofitInstance.kt
@@ -19,9 +19,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit 
 
 object RetrofitInstance {
-   private const val BASE_URL = "http://192.168.1.71:4000"
+//   private const val BASE_URL = "http://192.168.1.71:4000"
    //private const val BASE_URL = "https://healthcare-backend-yc39.onrender.com"
-//    private const val BASE_URL = "http://192.168.1.110:4000"
+    private const val BASE_URL = "http://192.168.0.101:4000"
     private val client = OkHttpClient.Builder()
         .connectTimeout(60, TimeUnit.SECONDS)  // Thời gian timeout kết nối
         .writeTimeout(60, TimeUnit.SECONDS)    // Thời gian timeout ghi dữ liệuz
