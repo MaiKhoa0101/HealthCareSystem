6a3bf5906b09548187b0c59addcebefd0dbf22f4
Phuc Le <lenguyenminhphuc0706@gmail.com>
2025-12-20 12:29:25 +0700

    adjust BMI checker screen, home screen

--- app/src/main/java/com/hellodoc/healthcaresystem/view/user/home/root/HomeScreen.kt
+++ app/src/main/java/com/hellodoc/healthcaresystem/view/user/home/root/HomeScreen.kt
@@ -59,7 +59,6 @@ import coil.request.ImageRequest
 import com.hellodoc.core.common.skeletonloading.SkeletonBox
 import com.hellodoc.healthcaresystem.R
 import com.hellodoc.healthcaresystem.model.dataclass.responsemodel.GetDoctorResponse
-import com.hellodoc.healthcaresystem.model.dataclass.responsemodel.GetMedicalOptionResponse
 import com.hellodoc.healthcaresystem.model.dataclass.responsemodel.GetSpecialtyResponse
 import com.hellodoc.healthcaresystem.model.dataclass.responsemodel.NewsResponse
 import com.hellodoc.healthcaresystem.model.dataclass.responsemodel.PostResponse
@@ -105,7 +104,6 @@ fun HealthMateHomeScreen(
 
     val doctorViewModel: DoctorViewModel = hiltViewModel()
     val specialtyViewModel: SpecialtyViewModel = hiltViewModel()
-    val medicalOptionViewModel: MedicalOptionViewModel = hiltViewModel()
     val geminiViewModel: GeminiViewModel = hiltViewModel()
     val postViewModel: PostViewModel = hiltViewModel()
     val newsViewModel: NewsViewModel = hiltViewModel()
@@ -115,7 +113,7 @@ fun HealthMateHomeScreen(
     // Collect states with loading information
     val doctorState by doctorViewModel.doctors.collectAsState()
     val specialtyState by specialtyViewModel.specialties.collectAsState()
-    val medicalOptionState by medicalOptionViewModel.medicalOptions.collectAsState()
+    val medicalOptions = listOf("Tính BMI", "Fast Talk")
     val question by geminiViewModel.question.collectAsState()
     val answer by geminiViewModel.answer.collectAsState()
     val newsState by newsViewModel.newsList.collectAsState()
@@ -146,8 +144,6 @@ fun HealthMateHomeScreen(
 
         doctorViewModel.fetchDoctors()
         specialtyViewModel.fetchSpecialties()
-        medicalOptionViewModel.fetchMedicalOptions()
-//        medicalOptionViewModel.fetchRemoteMedicalOptions()
         newsViewModel.getAllNews()
 //        if (ericModelInstance == null) {
 //            try {
@@ -264,29 +260,13 @@ fun HealthMateHomeScreen(
             }
 
             item(key = "services") {
-                if (medicalOptionState.isEmpty()) {
-//                    EmptyList("dịch vụ hệ thống")
-                    Column {
-                        SkeletonBox(
-                            modifier = Modifier
-                                .padding(horizontal = 16.dp)
-                                .fillMaxWidth(0.4f)
-                                .height(24.dp),
-                            shape = RoundedCornerShape(4.dp)
-                        )
-                        Spacer(modifier = Modifier.height(12.dp))
-                        ServiceSkeletonGrid()
-                    }
-                } else {
-                    SectionHeader(title = "Dịch vụ toàn diện")
-                    println("Dich vụ gồm: "+medicalOptionState)
-                    GridServiceList(medicalOptionState) { medicalOption ->
-                        when (medicalOption.name) {
-                            "Tính BMI" -> navHostController.navigate("bmi-checking")
-                            "Fast Talk" -> navHostController.navigate("fast_talk")
-                            else -> {
+                SectionHeader(title = "Dịch vụ toàn diện")
+                GridServiceList(medicalOptions) { optionName ->
+                    when (optionName) {
+                        "Tính BMI" -> navHostController.navigate("bmi-checking")
+                        "Fast Talk" -> navHostController.navigate("fast_talk")
+                        else -> {
 
-                            }
                         }
                     }
                 }
@@ -850,12 +830,12 @@ fun ServiceSkeletonGrid() {
 
 
 @Composable
-fun GridServiceList(items: List<GetMedicalOptionResponse>, onClick: (GetMedicalOptionResponse) -> Unit) {
+fun GridServiceList(items: List<String>, onClick: (String) -> Unit) {
     Column(modifier = Modifier.padding(horizontal = 16.dp)) {
         items.chunked(2).forEach { rowItems ->
             Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(16.dp)) {
                 rowItems.forEach { item ->
-                    key(item.id) {
+                    key(item) {
                         Box(
                             modifier = Modifier
                                 .weight(1f)
@@ -868,19 +848,19 @@ fun GridServiceList(items: List<GetMedicalOptionResponse>, onClick: (GetMedicalO
                             Row(verticalAlignment = Alignment.CenterVertically) {
                                 Image(
                                     painter = painterResource(id =
-                                        if (item.name == "Tính BMI") {
+                                        if (item == "Tính BMI") {
                                             R.drawable.doctor
                                         }
                                         else {
                                             R.drawable.speak
                                         }
                                     ),
-                                    contentDescription = item.name,
+                                    contentDescription = item,
                                     modifier = Modifier.size(40.dp),
                                     contentScale = ContentScale.Fit
                                 )
                                 Spacer(modifier = Modifier.width(3.dp))
-                                Text(text = item.name, color = MaterialTheme.colorScheme.onBackground)
+                                Text(text = item, color = MaterialTheme.colorScheme.onBackground)
                             }
                         }
                     }
